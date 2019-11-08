985bb9620142ab8080a734b1be96290a513d6b2d
carvalhojenni <jenniferc.dsr@gmail.com>
2019-11-08 11:54:41 -0300

    Resolvendo ex do Cep e POST

--- fetch/ex02/main.js
+++ fetch/ex02/main.js
@@ -1,14 +1,27 @@
 const cepInput = document.getElementById("cep")
-const logradouroInput = document.getElementById("logradouro")
+
+
+const form = document.createElement('form')
+    // const buscarCep = cepInput.form
     //... continuem a pegar os inputs do index.html para preencher depois com o valor da api
 
 cepInput.addEventListener("blur", () => {
-    if (cepInput.input) {
+    if (cepInput.value) {
         fetch(`https://viacep.com.br/ws/${cepInput.value}/json`).then(response => {
             return response.json()
         }).then(json => {
-            ///façam aparecer os valores nos inputs
-
+            console.log(json)
+            const logradouroInput = document.getElementById("logradouro")
+            logradouroInput.value = json.logradouro
+            const complementoInput = document.getElementById("complemento")
+            complementoInput.value = json.complemento
+            const bairroInput = document.getElementById("bairro")
+            bairroInput.value = json.bairro
+            const localidadeInput = document.getElementById("localidade")
+            localidadeInput.value = json.localidade
+            const ufInput = document.getElementById("uf")
+            ufInput.value = json.uf
+                ///façam aparecer os valores nos inputs
         })
     }
 })
